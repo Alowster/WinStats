16637b53345918e5d3ca8496286b7f82f431c075
Alowster <victoriosus2000@gmail.com>
2025-05-20 16:58:12 +0200

    v1.1.0

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
 import tkinter as tk
 import psutil
 
-def actualizar_cpu():
-    cpu = psutil.cpu_percent(interval=1)
-    label.config(text=f'C:{cpu}%')
-    root.after(200 , actualizar_cpu)
+def actualizar_metricas():
+    cpu = psutil.cpu_percent(interval=0.4)
+    match opcion.get():
+        case "1":
+            label.config(text=f'C:{cpu:02.0f}%')
+        case "2":
+            ram = psutil.virtual_memory().percent
+            label.config(text=f'C:{cpu:02.0f}% | R:{ram:02.0f}%')
+        case "X":
+            exit()
+    root.after(300 , actualizar_metricas)
 
 # Crear la ventana
 root = tk.Tk()
-root.title("Ventana Transparente con Texto")
-root.geometry("80x50+1080+0")
+root.geometry("200x50+1080+0")
 root.config(bg='black')
 root.overrideredirect(True)
 root.attributes("-transparentcolor", "black")
 root.attributes("-topmost", True)
 
-label = tk.Label(root, text='C:0%', font=("Arial", 11), fg="white", bg="black")
-label.pack(side="left")
+label = tk.Label(root, text='C:00%', font=("Arial", 11), fg="white", bg="black")
+label.place(x=45, y=0)
 
-button = tk.Button(root, text="X", command=root.destroy, bg="black", fg="white", bd=0 )
-button.pack(side="right")
+opcion = tk.StringVar(root)
+opcion.set("1")  # Valor por defecto
 
-actualizar_cpu()
+actualizar_metricas()
+menu = tk.OptionMenu(root, opcion, "1", "2", "X")
+menu.config(bg="black", fg="black", highlightthickness=0, bd=0, activebackground="gray")
+menu["menu"].config(bg="black", fg="white")  # Colores del menú desplegable
+menu.place(x=0, y=0)
 
 root.mainloop()
+
+
